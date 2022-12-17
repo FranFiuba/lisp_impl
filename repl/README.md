dfc7f6d4ccfc13982886036e21d3491b5aa4e95b
Francisco Strambini <franfiuba@gmail.com>
2022-12-17 11:28:11 -0300

    add repl and repl readme

---
+++ repl/README.md
@@ -0,0 +1,11 @@
+# Lisp Repl 
+
+A toy lisp repl implementation in C
+
+## pre-requisites
+
+sudo apt-get install libedit-dev 
+
+## compilation
+
+cc -std=c99 -Wall main.c -ledit -o repl
---
+++ repl/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main(int argc, char **argcv)
+{
+    puts("Lisp 0.0.0.1");
+    puts("Press Ctrl+c to Exit");
+
+    while (1)
+    {
+        /* output prompot */
+        char *input = readline("lisp> ");
+
+        /* get user input of max size 2048*/
+        add_history(input);
+
+        /* echo the user input */
+        printf("No you're a %s", input);
+
+        /* Free input memory*/
+        free(input);
+    }
+
+    return 0;
+}
